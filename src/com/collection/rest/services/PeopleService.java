26449ad7a634d1c6769b16c85e3e6b64f0fb6573
rajesh <veldandi.rajesh@gmail.com>
2017-01-30 23:07:48 -0600

    My Initial Commit

---
+++ src/com/collection/rest/services/PeopleService.java
@@ -0,0 +1,20 @@
+package com.collection.rest.services;
+
+import javax.ws.rs.Path;
+
+@Path("/people")
+public class PeopleService {
+	//getall
+	//getperson
+	//deleteperson
+	//edit
+	@Path("/all")
+	public void getAll(){
+		
+	}
+	
+	@Path("/add")
+	public void addPerson(){
+		
+	}
+}
